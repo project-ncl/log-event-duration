591c9204ac4a65d4345a8b17db9329705637cae7
Andrea Vibelli <avibelli@redhat.com>
2022-10-18 22:14:27 +0200

    [NCL-7332] Add Kafka Streams specific instrumentation

--- src/main/java/org/jboss/pnc/logprocessor/eventduration/Application.java
+++ src/main/java/org/jboss/pnc/logprocessor/eventduration/Application.java
@@ -1,11 +1,19 @@
 package org.jboss.pnc.logprocessor.eventduration;
 
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.Topology;
-
-import javax.enterprise.inject.Vetoed;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+
+import javax.enterprise.inject.Vetoed;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.processor.internals.DefaultKafkaClientSupplier;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.kafkaclients.KafkaTelemetry;
 
 /**
  * @author Ales Justin
@@ -24,8 +32,7 @@ public class Application {
         LogProcessorTopology logProcessorTopology = new LogProcessorTopology(inputTopic, outputTopic, durationsTopic);
 
         Topology topology = logProcessorTopology.buildTopology(kafkaProperties);
-        streams = new KafkaStreams(topology, kafkaProperties);
-
+        streams = new KafkaStreams(topology, kafkaProperties, new TracingKafkaClientSupplier());
     }
 
     public KafkaStreams.State getStreamState() {
@@ -39,6 +46,30 @@ public class Application {
     public void stop() {
         if (streams != null) {
             streams.close();
+        }
+    }
+
+    private static class TracingKafkaClientSupplier extends DefaultKafkaClientSupplier {
+        @Override
+        public Producer<byte[], byte[]> getProducer(Map<String, Object> config) {
+            KafkaTelemetry telemetry = KafkaTelemetry.create(GlobalOpenTelemetry.get());
+            return telemetry.wrap(super.getProducer(config));
+        }
+
+        @Override
+        public Consumer<byte[], byte[]> getConsumer(Map<String, Object> config) {
+            KafkaTelemetry telemetry = KafkaTelemetry.create(GlobalOpenTelemetry.get());
+            return telemetry.wrap(super.getConsumer(config));
+        }
+
+        @Override
+        public Consumer<byte[], byte[]> getRestoreConsumer(Map<String, Object> config) {
+            return this.getConsumer(config);
+        }
+
+        @Override
+        public Consumer<byte[], byte[]> getGlobalConsumer(Map<String, Object> config) {
+            return this.getConsumer(config);
         }
     }
 }
